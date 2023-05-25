c374837e7078f4202ad10ea65eccad9c9667e2a2
duyluandevil <71510988+duyluandevil@users.noreply.github.com>
2023-05-25 22:05:35 +0700

    chore: Adding comment to describe lifecycle of class

--- src/inversify.config.ts
+++ src/inversify.config.ts
@@ -4,7 +4,7 @@ import { IUser, User } from "./user/user";
 import { IManage, Manage } from "./manage";
 
 var container = new Container();
-container.bind<IUser>(Token.User).to(User).inTransientScope();
-container.bind<IManage>(Token.Manage).to(Manage).inSingletonScope();
+container.bind<IUser>(Token.User).to(User).inTransientScope(); // user just reset when has request to enforcements with it, so lifecycle is transient, does not need request
+container.bind<IManage>(Token.Manage).to(Manage).inSingletonScope(); // manage object is for managing all project so lifecycle of manage object just single tone
 
 export { container };
